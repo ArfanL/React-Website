192d848749152a4728d963d943b87cdfb4bd9ba9
Arfan Lokman <a_lokm@yahoo.com>
2020-10-30 10:58:07 -0400

    this is a new commit

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,7 +19,7 @@ class Projects extends Component {
                     }}> C minus Compiler</CardTitle>
                     <CardText>This is my project wrote in python that makes a compiler for c minus.</CardText>
                     <CardActions border>
-                        <Button colored>GitHub</Button>
+                        <a href="https://github.com/ArfanL/c-compiler"><Button colored>GitHub</Button></a>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share"/>
@@ -28,13 +28,27 @@ class Projects extends Component {
             )
         } else if (this.state.activeTab === 1) {
             return (
-                <div><h1>This is Call of Halo</h1></div>
-            )
-        } else if (this.state.activeTab === 2) {
-            return (
-                <div><h1>This is React</h1></div>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{
+                        color: '#fff',
+                        height: '176px',
+                        background: 'url(https://cdn.discordapp.com/attachments/710867865544163358/738208634734903406/-0pRdN01PmawfLWQyBD1oGcysMvDse7ONQ9ZmcZ64ax1g8e-3rBboTuqiGgm94VMqcuwpyxHdZCs8-NzbCYlFTfd7gTG_xte9ZXY.png) center / cover'
+                    }}> Call of Halo</CardTitle>
+                    <CardText>This is my First Person Shooter project that was made in Unity.</CardText>
+                    <CardActions border>
+                        <a href="https://github.com/ArfanL/Call-of-Halo"><Button colored>GitHub</Button></a>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
             )
         }
+        // else if (this.state.activeTab === 2) {
+        //     return (
+        //         <div><h1>This is React</h1></div>
+        //     )
+        // }
     }
 
     render() {
@@ -43,7 +57,7 @@ class Projects extends Component {
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
                     <Tab>Compilers</Tab>
                     <Tab>Call of Halo</Tab>
-                    <Tab>React</Tab>
+                    {/*<Tab>React</Tab>*/}
 
                 </Tabs>
                 <section className="projects-grid">
