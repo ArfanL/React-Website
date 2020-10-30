a5f3ce79439343b78f3cefe62df23090da82a2d0
Arfan Lokman <a_lokm@yahoo.com>
2020-10-29 20:47:17 -0400

    Updates

---
+++ precache-manifest.1fb070ce88dd590da2add8116fe6fd68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d056393f0ddfeb7c3f8c077457b37d3",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "1ec08da1f1c3cc123841",
+    "url": "/React-Website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e34ab381f60d6e889e84",
+    "url": "/React-Website/static/css/main.a2cacb0c.chunk.css"
+  },
+  {
+    "revision": "1ec08da1f1c3cc123841",
+    "url": "/React-Website/static/js/2.9d91bed8.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Website/static/js/2.9d91bed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34ab381f60d6e889e84",
+    "url": "/React-Website/static/js/main.dfaafe71.chunk.js"
+  },
+  {
+    "revision": "73f28a8d187045bdf660",
+    "url": "/React-Website/static/js/runtime-main.6707cdd9.js"
+  }
+]);
